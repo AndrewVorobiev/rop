96c110f4e134af5ac1a7901c11822444ee6a2fd4
andrew <andrew.vorobiev.work@gmail.com>
2023-03-01 19:14:31 +0300

    Socket

--- app/src/main/java/com/test/rop/MainActivity.kt
+++ app/src/main/java/com/test/rop/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.test.rop
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.test.rop.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
@@ -8,57 +10,64 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.*
 import java.net.Socket
+import java.nio.charset.Charset
 import java.util.Scanner
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreferences: SharedPreferences
 
-    private var active: Boolean = false
-    private var data: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val address = "194.87.99.239"
-        val port = 80
-        binding.buttonConnect.setOnClickListener{
-            if(binding.buttonConnect.text == "connect"){
-                binding.buttonConnect.text = "disconnect"
-                active = true
-                CoroutineScope(Dispatchers.IO).launch {
-                    client(address,port)
+
+        try{
+            val ip = "193.124.118.129"
+            val socket = Socket(ip, 80)
+
+            val out = socket.getOutputStream()
+
+            if (sharedPreferences.getBoolean("isNormal", false)) {
+                out.write("normal".toByteArray(Charsets.UTF_8))
+            }
+
+
+            val br = BufferedReader(InputStreamReader(socket.getInputStream()))
+            var cmd: String
+            var C2S_Start = 0L
+            while (br.readLine().also { cmd = it } != null) {
+                Log.e("While", "Start")
+                //Log.e("While",cmd)
+                when (cmd) {
+                    "ping" -> {
+                        out.write("pong".toByteArray(Charsets.UTF_8))
+                        out.flush()
+                        Thread.sleep(50)
+                        C2S_Start = System.currentTimeMillis()
+                        out.write("ping".toByteArray(Charsets.UTF_8))
+                    }
+                    "pong" -> {
+                        val ping = System.currentTimeMillis() - C2S_Start
+                        out.write(("*$ping").toByteArray(Charsets.UTF_8))
+                        Thread.sleep(40)
+                    }
+
+                    else -> {
+//                    handleCommand(cmd, sharedPreferences)
+                        Log.e("cmd", cmd)
+                    }
                 }
-            } else {
-                active = false
-                binding.buttonConnect.text = "connect"
             }
-
-        }
-
     }
-    private suspend fun client(address: String, port: Int){
-        val connection = Socket(address, port)
-        val writer = connection.getOutputStream()
-        writer.write(1)
-        val reader = Scanner(connection.getInputStream())
-        while(active){
-            var input = ""
-            input = reader.nextLine()
-            if(data.length < 300) {
-                data += "\n$input"
-            }
-            else{
-                data = input
-                binding.tvServerResponse.text = data
-            }
-            reader.close()
-            writer.close()
-            connection.close()
+        catch (e: IOException) {
+            e.message?.let { Log.e("While", it) };
         }
     }
 }
+
 
 
 
